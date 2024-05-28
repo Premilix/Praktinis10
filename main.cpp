aadb963860428b0e5e287751d26611e426f5ae0c
Premilix <46068502+Premilix@users.noreply.github.com>
2024-05-28 09:59:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <bits/stdc++.h>
 
+// struct zenklas
+// struct kolekcionierius
+// duomenu sukurimas
+// ivedimas is failo
+// pradiniai i faila
+// rezultatai i faila
+// populiariausių pašto ženklų sąrašo be pasikartojimų funkciją
+// funkcija, kuri suranda asmenis, neturinčius nurodyto pašto ženklo ir surašo juos į naują sąrašą.
+// rikiavimas
+// main
+
 using namespace std;
 
 int main()
