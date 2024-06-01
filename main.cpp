279daf6c8b97f0828fffee44abe4d2a3ab772c89
Premilix <46068502+Premilix@users.noreply.github.com>
2024-06-01 20:19:43 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 // pradiniai i faila (Motiejus)
 // rezultatai i faila (Motiejus)
 // populiariausių pašto ženklų sąrašo be pasikartojimų funkciją (Augustas)
-// funkcija, kuri suranda asmenis, neturinčius nurodyto pašto ženklo ir surašo juos į naują sąrašą. (Emilis)
+// funkcija, kuri suranda asmenis, neturinčius nurodyto pašto ženklo, ir surašo juos į naują sąrašą. (Emilis)
 // rikiavimas (Augustas)
 
 using namespace std;
@@ -20,11 +20,32 @@ struct Zenklas
 struct Kolekcionierius
 {
     string varPav;
-    string pavadinimas;
-    int kiekis;
+    string pavadinimas[256];
+    int kiekis[256];
 };
+
+void Neturintys(Kolekcionierius K[], Kolekcionierius Naujas[], int n, int &m, Zenklas zenklas);
+bool ArTuri(Kolekcionierius k, Zenklas zenklas);
 
 int main()
 {
+    
+}
 
+// funkcija, kuri suranda asmenis, neturinčius nurodyto pašto ženklo, ir surašo juos į naują sąrašą
+void Neturintys(Kolekcionierius K[], Kolekcionierius Naujas[], int n, int &m, Zenklas zenklas) {
+    m = 0;
+    for (int i = 0; i < n; i++) {
+        if (!ArTuri(K[i], zenklas)) {
+            Naujas[m++] = K[i];
+        }
+    }
+}
+
+// funkcija, kuri tikrina, ar asmuo turi nurodytą pašto ženklą
+bool ArTuri(Kolekcionierius k, Zenklas zenklas) {
+    for (int i = 0; i < k.nr; i++) {
+        if (k.pavadinimas[i] == zenklas.pavadinimas) return true;
+    }
+    return false;
 }
